5b7d4d1bb2aacec32eb14b49eb44343f5317dc2b
mathewsecure <mateosegurag845@gmail.com>
2025-07-25 22:07:43 -0600

    Update JWT token expiration time

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -14,7 +14,7 @@ const doLogin = async (req, res) => {
 
     if (rows[0].quantity === 1) {
       token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: 3600000,
+        expiresIn: 60,
       });
       result = { token: token, message: "User authenticated" };
     } else {
