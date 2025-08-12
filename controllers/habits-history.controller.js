34874ccbdf4976edc255cd402cbff0df916caa7a
mathewsecure <mateosegurag845@gmail.com>
2025-08-11 23:22:07 -0600

    Update insertCompletionChecks for testing

--- controllers/habits-history.controller.js
+++ controllers/habits-history.controller.js
@@ -1,14 +1,27 @@
 import { pool } from "../helpers/mysql-config.js";
 
 const insertCompletionChecks = async (req, res) => {
-  res.status(201);
+  try {
+    const { email } = req.email;
+    const [user_id] = await pool.query("SELECT id FROM users WHERE email=?", [
+      email,
+    ]);
+    const [habit_ids] = await pool.query(
+      "SELECT id FROM habits WHERE user_id=?;",
+      [user_id[0].id]
+    );
+    const [result] = await pool.query("", []);
+    res.status(201).json({ affectedRows: result.affectedRows });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error at inserting completion checks" });
+  }
 };
 const selectCompletionChecks = async (req, res) => {
-  res.status(201);
-  console.log("hello");
+  res.send("hello");
 };
 const updateCompletionCheck = async (req, res) => {
-  res.status(201);
+  res.send("hello");
 };
 
 export {
