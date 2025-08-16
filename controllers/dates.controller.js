be248a48606316b12a3996b8540b346b19551eb6
mathewsecure <mateosegurag845@gmail.com>
2025-08-15 23:58:19 -0600

    Add dates controller

---
+++ controllers/dates.controller.js
@@ -0,0 +1,9 @@
+const selectDates = async (req, res) => {
+  res.send("hello");
+};
+
+const insertDates = async (req, res) => {
+  res.send("hello");
+};
+
+export { selectDates, insertDates };
---
+++ routes/dates.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import { insertDates, selectDates } from "../controllers/dates.controller.js";
+import { middleware } from "../middleware/jwt.middleware.js";
+
+router.get("/", middleware, selectDates);
+router.post("/", middleware, insertDates);
+
+export { router };
