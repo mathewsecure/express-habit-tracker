3aa6e48417febc675b66fd135910b437c441267a
mathewsecure <mateosegurag845@gmail.com>
2025-08-17 23:52:27 -0600

    Update dates controller to add its functionality

--- controllers/dates.controller.js
+++ controllers/dates.controller.js
@@ -1,9 +1,24 @@
+import { pool } from "../helpers/mysql-config.js";
+
 const selectDates = async (req, res) => {
-  res.send("hello");
+  try {
+    const { email } = req.email;
+    const [user_id] = await pool.query("SELECT id FROM users WHERE email=?", [
+      email,
+    ]);
+    const [dates] = await pool.query(
+      "SELECT date FROM dates WHERE user_id=? ORDER BY date ASC",
+      [user_id[0].id]
+    );
+    res.json({ dates });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error at getting dates" });
+  }
 };
 
 const insertDates = async (req, res) => {
-  res.send("hello");
+  res.send("post");
 };
 
 export { selectDates, insertDates };
