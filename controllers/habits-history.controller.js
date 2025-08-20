778e4c0e83fa0a5564c1690e6cd3e7797d39e4da
mathewsecure <mateosegurag845@gmail.com>
2025-08-19 23:45:33 -0600

    Update insertCompletionChecks to insert body's date

--- controllers/habits-history.controller.js
+++ controllers/habits-history.controller.js
@@ -2,25 +2,26 @@ import { pool } from "../helpers/mysql-config.js";
 
 const insertCompletionChecks = async (req, res) => {
   try {
-    const datePassedFromFrontend = "Wed Aug 13 2025";
     const { email } = req.email;
     const [user_id] = await pool.query("SELECT id FROM users WHERE email=?", [
       email,
     ]);
+    const { date } = req.body;
+    if (!date) {
+      return res.status(400).json({ error: "Enter all fields" });
+    }
     const [habit_ids] = await pool.query(
       "SELECT id FROM habits WHERE user_id=?;",
       [user_id[0].id]
     );
+    /*
+    select the date that you want to insert
+    if rows = 0
+      execute results
+    */
     const [result] = await pool.query(
       "INSERT INTO completion_history (completion_check, date,user_id,habit_id) VALUES ?",
-      [
-        habit_ids.map((habit_id) => [
-          0,
-          datePassedFromFrontend,
-          user_id[0].id,
-          habit_id.id,
-        ]),
-      ]
+      [habit_ids.map((habit_id) => [0, date, user_id[0].id, habit_id.id])]
     );
     res.status(201).json({ affectedRows: result.affectedRows });
   } catch (error) {
