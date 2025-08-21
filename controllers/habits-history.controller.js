16d67103f6796327d5b2dab0d3d4795aac06c469
mathewsecure <mateosegurag845@gmail.com>
2025-08-20 23:11:55 -0600

    Update insertCompletionChecks to only allow date insertions when they dont exist. (Need to fix: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client)

--- controllers/habits-history.controller.js
+++ controllers/habits-history.controller.js
@@ -10,20 +10,22 @@ const insertCompletionChecks = async (req, res) => {
     if (!date) {
       return res.status(400).json({ error: "Enter all fields" });
     }
-    const [habit_ids] = await pool.query(
-      "SELECT id FROM habits WHERE user_id=?;",
-      [user_id[0].id]
+    const [date_existance] = await pool.query(
+      "SELECT 1 FROM completion_history WHERE date=? AND user_id=?",
+      [date, user_id[0].id]
     );
-    /*
-    select the date that you want to insert
-    if rows = 0
-      execute results
-    */
-    const [result] = await pool.query(
-      "INSERT INTO completion_history (completion_check, date,user_id,habit_id) VALUES ?",
-      [habit_ids.map((habit_id) => [0, date, user_id[0].id, habit_id.id])]
-    );
-    res.status(201).json({ affectedRows: result.affectedRows });
+    if (date_existance.length < 1) {
+      const [habit_ids] = await pool.query(
+        "SELECT id FROM habits WHERE user_id=?;",
+        [user_id[0].id]
+      );
+      const [result] = await pool.query(
+        "INSERT INTO completion_history (completion_check, date,user_id,habit_id) VALUES ?",
+        [habit_ids.map((habit_id) => [0, date, user_id[0].id, habit_id.id])]
+      );
+      res.status(201).json({ affectedRows: result.affectedRows });
+    }
+    res.status(201).json({ affectedRows: 0 });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Error at inserting completion checks" });
